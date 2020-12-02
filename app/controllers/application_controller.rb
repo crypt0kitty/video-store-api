e00d7ef0ff955b2864a5c0d3a92ec06db0c9aca9
Mackenzie <mvlofthus@gmail.com>
2020-12-01 17:40:28 -0800

    test route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::API
   def zomg
-    videos = Video.all
-
-    render json: videos.as_json(only: [:id, :name, :age, :owner, :species]),
-           message: "it works!"
+    # videos = Video.all
+    #
+    # render json: videos.as_json(only: [:id, :name, :age, :owner, :species]),
+    #        message: "it works!"
+    #
+    render json: { message: "it works!" }, status: :ok
   end
 end
