340c750dc0b95688b42bd74403b7b74f256de25f
Sandy <sandyvasquez9@gmail.com>
2020-12-01 17:35:06 -0800

    wave 0

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::API
+  def zomg
+    videos = Video.all
+
+    render json: videos.as_json(only: [:id, :name, :age, :owner, :species]),
+           message: "it works!"
+  end
+end
